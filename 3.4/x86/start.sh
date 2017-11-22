af302d8e7b7fa57118902ba7f2e97b27ad70ffba
Dashlorde <zhouyunlu0216@gmail.com>
2017-11-22 04:01:46 -0500

    add moodle

--- 3.4/x86/start.sh
+++ 3.4/x86/start.sh
@@ -1,10 +1,5 @@
 #!bin/bash
 
-chown -R www-data:www-data /moodle/moodledata
-
-rm -rf /var/www/html/config.php
-cp ./config.php /var/www/html/config.php
-
 source /etc/apache2/envvars
 tail -F /var/log/apache2/* &
 exec apache2 -D FOREGROUND
