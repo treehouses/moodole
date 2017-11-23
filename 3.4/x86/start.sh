2457748f6d70b52d78005297066d1f7c6ae606be
Yunlu Zhou <zhouyunlu0216@gmail.com>
2017-11-22 22:38:25 -0500

    Package moodle in proper way (#2)

--- 3.4/x86/start.sh
+++ 3.4/x86/start.sh
@@ -1,11 +1,6 @@
 #!bin/bash
 
-chown -R www-data:www-data /var/moodledata
-chmod 777 /var/moodledata
-
-rm -rf /var/www/html/config.php
-cp ./config.php /var/www/html/config.php
-
 source /etc/apache2/envvars
 tail -F /var/log/apache2/* &
+
 exec apache2 -D FOREGROUND
