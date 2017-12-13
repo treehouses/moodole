3b43328c4bcaccb8bdea35cbdc6fb9b1bff06af3
mappuji <mappuji@protonmail.com>
2017-12-13 22:37:17 +0700

    Add initial travis infra code 7

--- .travis/deploy_docker.sh
+++ .travis/deploy_docker.sh
@@ -2,4 +2,10 @@
 
 source ./.travis/travis_utils.sh
 
-build_message Under construction...
+build_message Build x86 image started...
+
+build_message Buil x86 image finished, check build result!
+
+build_message Build ARM image started...
+
+build_message Buil ARM image finished, check build result!
