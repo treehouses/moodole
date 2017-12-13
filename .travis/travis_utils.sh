1ca465ead3a8020695724a762f3fbbb7352985a2
mappuji <mappuji@protonmail.com>
2017-12-13 22:10:42 +0700

    Add initial travis infra code

---
+++ .travis/travis_utils.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+build_message(){
+    # $1 = build message
+    echo
+    echo =========BUILD MESSAGE=========
+    echo "$@"
+    echo ===============================
+    echo
+}
